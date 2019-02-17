10a53c1c83b23a19cfbd6f3db37a446ff6eb5d74
Toni Karppi <karppitoni@gmail.com>
2019-02-17 02:40:06 +0100

    Bump to 0.0.2

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", encoding="utf8") as f:
 
 setup(
     name="yks",
-    version="0.0.1",
+    version="0.0.2",
     description="A sample project",
     long_description=readme,
     long_description_content_type="text/markdown",
